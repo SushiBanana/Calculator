3717fe471b1119649a08d5a761ba9488f908538e
SushiBanana <alysha.thean@gmail.com>
2023-07-06 16:03:24 +0800

    Implement initial calculator design

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hi")
+const zeroButton = document.querySelector(".zero-button")
+const calculatorDisplay = document.querySelector(".calculator-display")
+
+zeroButton.addEventListener("click", () => {
+    const content = document.createElement('div')
+    content.textContent = "0"
+    calculatorDisplay.appendChild(content)
+})
