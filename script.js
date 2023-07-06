8ef4ae1ec4d80fbfdf92c824a6adabcf44c3d743
SushiBanana <alysha.thean@gmail.com>
2023-07-06 21:46:12 +0800

    Add backspace and numbers interactivity

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-const zeroButton = document.querySelector(".zero-button")
 const calculatorDisplay = document.querySelector(".calculator-display")
+const buttonsContainer = document.querySelector(".buttons-container")
+const backspaceButton = document.querySelector(".backspace-button")
+const divideButton = document.querySelector(".divide-button")
+const timesButton = document.querySelector(".times-button")
+const minusButton = document.querySelector(".minus-button")
+const plusButton = document.querySelector(".plus-button")
 
-zeroButton.addEventListener("click", () => {
-    const content = document.createElement('div')
-    content.textContent = "0"
-    calculatorDisplay.appendChild(content)
+function createGlobalEventListener(type, selector, callback) {
+    buttonsContainer.addEventListener(type, e => {
+        if(e.target.matches(selector)) callback(e)
+    })
+}
+
+createGlobalEventListener("click", ".number", e => {
+    let numberValue = e.target.textContent
+    let displayer = document.createElement("div")
+    displayer.textContent = numberValue
+    calculatorDisplay.appendChild(displayer)
+
+})
+backspaceButton.addEventListener("click", e => {
+    if (e.target.matches("button") || e.target.matches(".fas")) {
+        calculatorDisplay.removeChild(calculatorDisplay.lastChild)
+    }
+})
+
+backspaceButton.addEventListener("click", e => {
+    if (e.target.matches("button") || e.target.matches(".fas")) {
+        calculatorDisplay.removeChild(calculatorDisplay.lastChild)
+    }
 })
